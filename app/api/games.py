b705063c4c05b99fb9d500d504a1bb11a8de1995
rk.dev <rekubota.dev@gmail.com>
2026-05-20 10:27:17 +0900

    ゲームごとの価格履歴取得APIを追加

--- app/api/games.py
+++ app/api/games.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException, Query
 from app.db import SessionLocal
-from app.models import Game
+from app.models import Game, PriceHistory
 
 
 # Router
@@ -52,4 +52,40 @@ def get_game(game_id: int):
         "title": game.title,
         "price": game.price,
         "created_at": game.created_at
+    }
+    
+    
+# ゲーム別価格履歴
+@router.get("/{game_id}/history")
+def get_game_histiry(game_id: int):
+    session = SessionLocal()
+    
+    # ゲーム存在確認
+    game = session.query(Game).filter(Game.id == game_id).first()
+    
+    if game is None:
+        session.close()
+        
+        raise HTTPException(status_code=404, detail="ゲームが見つかりません")
+    
+    # 履歴取得
+    histories = session.query(PriceHistory).filter(PriceHistory.game_id == game_id).all()
+    
+    result = []
+    
+    for history in histories:
+        result.append({
+            "id": history.id,
+            "price": history.price,
+            "created_at": history.created_at
+        })
+        
+    session.close()
+    
+    return {
+        "game": {
+            "id": game.id,
+            "title": game.title
+        },
+        "history": result
     }
